b6def4ecefe76973067f0041f144bd47dfaa3ba5
LAURA PATRICIA CRUZ <95201770+LauraPatriciaCruz@users.noreply.github.com>
2024-07-10 14:27:17 -0300

    .

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,3 @@
-// const mysql = require('mysql2');
-
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Mysql555',
-//     database: 'sociedad_protectora_db'
-// });
-
-// connection.connect(error => {
-//     if (error) throw error;
-//     console.log('Conectado a sociedad_protectora_db');
-// });
-
-// module.exports = connection;
-
-
-
 const mysql = require('mysql2');
 require('dotenv').config();
 
